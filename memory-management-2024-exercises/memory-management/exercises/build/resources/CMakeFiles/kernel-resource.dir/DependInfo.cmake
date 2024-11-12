88c440ec338afe9f94e5eb2ae7cf1fe97ec918ab
ShrikeBin <grahamka.ha@gmail.com>
2024-11-12 14:48:17 +0100

    paro duże zadanie 5

---
+++ memory-management-2024-exercises/memory-management/exercises/build/resources/CMakeFiles/kernel-resource.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/h20/Documents/VSC/Paro/memory-management-2024-exercises/memory-management/exercises/resources/src/kernel_resource.cpp" "resources/CMakeFiles/kernel-resource.dir/src/kernel_resource.cpp.o" "gcc" "resources/CMakeFiles/kernel-resource.dir/src/kernel_resource.cpp.o.d"
+  "/home/h20/Documents/VSC/Paro/memory-management-2024-exercises/memory-management/exercises/resources/src/printers_old.cpp" "resources/CMakeFiles/kernel-resource.dir/src/printers_old.cpp.o" "gcc" "resources/CMakeFiles/kernel-resource.dir/src/printers_old.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ memory-management-2024-exercises/memory-management/exercises/build/resources/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/h20/Documents/VSC/Paro/memory-management-2024-exercises/memory-management/exercises/resources
+# Build directory: /home/h20/Documents/VSC/Paro/memory-management-2024-exercises/memory-management/exercises/build/resources
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
